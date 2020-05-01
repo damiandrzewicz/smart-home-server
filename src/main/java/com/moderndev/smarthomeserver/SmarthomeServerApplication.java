c36e9ae63b394d8567d854d01b1dbe6e15dbb514
Damian <damian.drzewicz@gmail.com>
2020-05-01 12:02:54 +0200

    Consistency bugfix.

--- src/main/java/com/moderndev/smarthomeserver/SmarthomeServerApplication.java
+++ src/main/java/com/moderndev/smarthomeserver/SmarthomeServerApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SmarthomeServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SmarthomeApplication.class, args);
+		SpringApplication.run(SmarthomeServerApplication.class, args);
 	}
 
 }
